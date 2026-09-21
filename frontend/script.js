9d2c3793faa3b39c30d1f7e07ea6bf925f651a5b
satyalamanasa10 <satyalamanasa10@gmail.com>
2026-09-21 21:16:16 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 // SMART LOAN AI - FRONTEND JAVASCRIPT
 // ============================================================
 
-const API_URL = "http://127.0.0.1:8000/predict";
+const API_URL = "http://smart-loan-risk-prediction.onreader.com/predict";
 
 
 // ============================================================
